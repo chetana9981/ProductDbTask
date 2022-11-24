b22f63be6e97ecbb8a2b6932065d1103031c1406
chetana9981 <101543143+chetana9981@users.noreply.github.com>
2022-11-24 17:50:10 +0530

    Add files via upload

---
+++ ProductDatabaseTask/src/main/kotlin/com/axis/service/ProductService.kt
@@ -0,0 +1,27 @@
+package com.axis.service
+
+import com.axis.model.Product
+import com.axis.utility.ProductUtil
+
+class ProductService {
+
+
+        var productUtil= ProductUtil()
+
+        fun insertProduct(){
+            var res =productUtil.insert()
+            println(res)
+        }
+
+        fun displayAllProducts(){
+            productUtil.selectAllProducts()
+        }
+
+        fun insertUserProduct(product: Product){
+            productUtil.insertUserInput(product)
+        }
+
+
+    }
+
+
